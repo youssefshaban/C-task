a43d31c10d81c60254b4e1a40c63c22872ae4b1d
youssefshaban <youssef.shaban.atta@gmail.com>
2020-10-21 00:28:31 +0200

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+/*
+ 
+ الفكرة في انك تستخدم variable واحد هوا التعريف بتاعها may change
+    int x;
+ هنا x بواحد
+    x = 1;
+ بس ممكن اغيرها
+    x=2;
+ x بتاخد اخر قيمة
+ note :
+ **** const cant be change ****
+ const int x = 1;
+ x=9; // Error
+ 
+ 
+ */
+
+int main (){
+int input, max,min;
+
+
+//get the first number
+printf("enter the number: ");
+scanf("%d",&input);
+
+// check for max and min number
+if (input>max){
+      max= input;
+}
+
+if (input<min){
+	min=input;
+}
+
+//get number 2
+printf("enter a number :");
+scanf("%d",&input);
+
+//check for max and min number
+if (input>max){
+	max = input;
+}
+if (input < min ){
+	min = input;
+}
+
+//get number 3
+printf("enter a number : ");
+scanf ("%d",&input);
+// check for max and min number
+if (input > max ){
+	max=input;
+}
+if (input < min){
+	min=input;
+}
+    
+    
+//get number 4
+printf("enter a number : ");
+scanf ("%d",&input);
+// check for max and min number
+if (input > max ){
+	max=input;
+}
+if (input < min){
+	min=input;
+}
+    
+    
+//get number 5
+printf("enter a number : ");
+scanf ("%d",&input);
+// check for max and min number
+if (input > max ){
+	max=input;
+}
+if (input < min){
+	min=input;
+}
+
+printf("the max number is : %d\nthe min number is : %d",max,min);
+
+return 0;
+}
